03a0aa31e477277ca180fe1e88aa62ee1cde36c8
cheesycod <cheesycod@outlook.com>
2023-11-30 17:51:16 +0530

    now it runs

--- config/config.go
+++ config/config.go
@@ -50,26 +50,20 @@ type Config struct {
 	Notifications Notifications `yaml:"notifications" validate:"required"`
 	Servers       Servers       `yaml:"servers" validate:"required"`
 	Meta          Meta          `yaml:"meta" validate:"required"`
-	Hcaptcha      Hcaptcha      `yaml:"hcaptcha" validate:"required"`
-}
-
-type Hcaptcha struct {
-	SiteKey string `yaml:"site_key" comment:"Hcaptcha Site Key" validate:"required"`
-	Secret  string `yaml:"secret" comment:"Hcaptcha Secret" validate:"required"`
 }
 
 type DiscordAuth struct {
 	Token            string   `yaml:"token" comment:"Discord bot token" validate:"required"`
-	ClientID         string   `yaml:"client_id" default:"815553000470478850" comment:"Discord Client ID" validate:"required"`
+	ClientID         string   `yaml:"client_id" default:"849331145862283275" comment:"Discord Client ID" validate:"required"`
 	ClientSecret     string   `yaml:"client_secret" comment:"Discord Client Secret" validate:"required"`
-	AllowedRedirects []string `yaml:"allowed_redirects" default:"http://localhost:3000/auth/sauron,http://localhost:8000/auth/sauron,https://reedwhisker.infinitybots.gg/auth/sauron,https://infinitybots.gg/auth/sauron,https://botlist.site/auth/sauron,https://infinitybots.xyz/auth/sauron" validate:"required"`
+	AllowedRedirects []string `yaml:"allowed_redirects" default:"http://localhost:3000/auth" validate:"required"`
 }
 
 type Sites struct {
-	Frontend Differs[string] `yaml:"frontend" default:"https://reedwhisker.infinitybots.gg" comment:"Frontend URL" validate:"required"`
-	API      Differs[string] `yaml:"api" default:"https://spider.infinitybots.gg" comment:"API URL" validate:"required"`
-	CDN      string          `yaml:"cdn" default:"https://cdn.infinitybots.gg" comment:"CDN URL" validate:"required"`
-	Instatus string          `yaml:"instatus" default:"https://infinity-bots.instatus.com" comment:"Instatus Status Page URL" validate:"required"`
+	Frontend Differs[string] `yaml:"frontend" default:"https://antiraid.xyz" comment:"Frontend URL" validate:"required"`
+	API      Differs[string] `yaml:"api" default:"https://splashtail.antiraid.xyz" comment:"API URL" validate:"required"`
+	CDN      string          `yaml:"cdn" default:"https://cdn.antiraid.xyz" comment:"CDN URL" validate:"required"`
+	Instatus string          `yaml:"instatus" default:"https://status.antiraid.xyz" comment:"Instatus Status Page URL" validate:"required"`
 }
 
 type Roles struct {
@@ -83,22 +77,21 @@ type JAPI struct {
 }
 
 type Notifications struct {
-	VapidPublicKey  string `yaml:"vapid_public_key" default:"BIdUNSqYzqVjbdJhn8WK6SDYDVj85mKtctrEgj14KkjxIMerxQ9wywvvxECkuP8rL3s8zDgZSE9HSqW1wmhVPM8" comment:"Vapid Public Key (https://www.stephane-quantin.com/en/tools/generators/vapid-keys)" validate:"required"`
+	VapidPublicKey  string `yaml:"vapid_public_key" default:"BNMhOWvN-6_jm72D3Ncnxmvwz03TLDNVOi1bd9uD-OjWbHmaa4w1A5nq8MTjSKL_tnMueI64ZxtRXWRltRu0Vio" comment:"Vapid Public Key (https://www.stephane-quantin.com/en/tools/generators/vapid-keys)" validate:"required"`
 	VapidPrivateKey string `yaml:"vapid_private_key" comment:"Vapid Private Key (https://www.stephane-quantin.com/en/tools/generators/vapid-keys)" validate:"required"`
 }
 
 type Servers struct {
-	Main string `yaml:"main" default:"758641373074423808" comment:"Main Server ID" validate:"required"`
+	Main string `yaml:"main" default:"1064135068928454766" comment:"Main Server ID" validate:"required"`
 }
 
 type Meta struct {
-	PostgresURL         string          `yaml:"postgres_url" default:"postgresql:///infinity" comment:"Postgres URL" validate:"required"`
-	RedisURL            Differs[string] `yaml:"redis_url" default:"redis://localhost:6379" comment:"Redis URL" validate:"required"`
-	Port                Differs[string] `yaml:"port" default:":8081" comment:"Port to run the server on" validate:"required"`
-	CDNPath             string          `yaml:"cdn_path" default:"/failuremgmt/cdn/antiraid" comment:"CDN Path" validate:"required"`
-	SecureStorage       string          `yaml:"secure_storage" default:"/failuremgmt/sec/antiraid" comment:"Blob Storage URL" validate:"required"`
-	VulgarList          []string        `yaml:"vulgar_list" default:"fuck,suck,shit,kill" validate:"required"`
-	UrgentMentions      string          `yaml:"urgent_mentions" default:"<@&1061643797315993701>" comment:"Urgent mentions" validate:"required"`
-	UptimeRobotROAPIKey string          `yaml:"uptime_robot_ro_api_key" default:"" comment:"Uptime Robot Read-Only API Key" validate:"required"`
-	PopplioProxy        string          `yaml:"popplio_proxy" default:"http://127.0.0.1:3219" comment:"Popplio Proxy URL" validate:"required"`
+	PostgresURL    string          `yaml:"postgres_url" default:"postgresql:///antiraid" comment:"Postgres URL" validate:"required"`
+	RedisURL       Differs[string] `yaml:"redis_url" default:"redis://localhost:6379" comment:"Redis URL" validate:"required"`
+	Port           Differs[string] `yaml:"port" default:":8081" comment:"Port to run the server on" validate:"required"`
+	CDNPath        string          `yaml:"cdn_path" default:"/failuremgmt/cdn/antiraid" comment:"CDN Path" validate:"required"`
+	SecureStorage  string          `yaml:"secure_storage" default:"/failuremgmt/sec/antiraid" comment:"Blob Storage URL" validate:"required"`
+	VulgarList     []string        `yaml:"vulgar_list" default:"fuck,suck,shit,kill" validate:"required"`
+	UrgentMentions string          `yaml:"urgent_mentions" default:"<@&1061643797315993701>" comment:"Urgent mentions" validate:"required"`
+	Proxy          string          `yaml:"proxy" default:"http://127.0.0.1:3219" comment:"Popplio Proxy URL" validate:"required"`
 }
