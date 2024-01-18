850b26f7cabd13b52c806e121d9cbd628def7b2d
a <a>
2024-01-18 07:34:34 +0000

    add restore op

--- botv2/src/cmds/backups/mod.rs
+++ botv2/src/cmds/backups/mod.rs
@@ -7,6 +7,7 @@ pub fn commands() -> Vec<super::CommandAndPermissions> {
             "" => super::CommandExtendedData::kittycat_simple("backups", "*"),
             "create" => super::CommandExtendedData::kittycat_or_admin("backups", "create"),
             "list" => super::CommandExtendedData::kittycat_or_admin("backups", "list"),
+            "restore" => super::CommandExtendedData::kittycat_or_admin("backups", "restore"),
         }),
     ]
 }
