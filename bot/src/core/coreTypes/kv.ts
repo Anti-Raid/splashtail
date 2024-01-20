ca751a2ae880ef8c1f0da7bb93fe54725a0e0499
a <a>
2024-01-20 05:27:51 +0000

    remove old js code

--- bot/src/core/coreTypes/kv.ts
+++
@@ -1,1 +0,0 @@
-export type KV = { [key: string]: any }
--- bot/src/core/db.ts
+++
@@ -1,9 +0,0 @@
-import postgres from 'postgres'
-
-const sql = postgres({ 
-    database: 'antiraid',
-    transform: postgres.fromCamel,
-    password: 'password',
- })
-
-export default sql
