b47aa27fb98ebc6ca110fad982d3eb3529bc77da
kakarot <kakarot.joel@gmail.com>
2024-02-15 17:36:01 +0000

    small changes, and testing left

--- botv2/src/modules/limits/handler.rs
+++ botv2/src/modules/limits/handler.rs
@@ -48,7 +48,7 @@ use super::core;
         return Ok(true); // Ignore
     }
 
-    Ok(false) // Don't ignore 
+    Ok(false) // Don't ignore
 }**/
 
 pub struct HandleModAction {
@@ -83,13 +83,12 @@ pub async fn handle_mod_action(
             .filter(|a| a.limit_type == limit)
             .map(|a| (a.limit_id.clone(), a))
             .collect()
-
     };
     if guild_limits.is_empty() {
         // No limits for this guild
         return Ok(());
     }
-    for (limit_id, limit) in guild_limits.into_iter() {
+    for (limit_id, guild_limit) in guild_limits.into_iter() {
         // Check the limit type and user_id and guild to see if it is in the cache
         let mut query = cache.query("select * from user_actions where guild_id=type::string($guild_id) and user_id=type::string($user_id) and limit_type=type::string($limit_type)")
             .bind(("guild_id", guild_id.clone()))
@@ -97,213 +96,110 @@ pub async fn handle_mod_action(
             .bind(("limit_type", limit.to_string()))
             .await?;
         let response: Vec<UserAction> = query.take(0)?;
-        // if response.len() >
-        // Add to cache
-        let req: Vec<UserAction> = cache.create("user_actions")
-            .content(UserAction {
-                action_id: crate::impls::crypto::gen_random(48),
-                guild_id: guild_id,
-                user_id: user_id,
-                target: target.clone(),
-                limit_type: limit,
-                action_data: action_data.clone(),
-                created_at: sqlx::types::chrono::Utc::now(),
-            })
-            .await?;
-    }
-    // Add to GUILD_MEMBER_ACTIONS_CACHE
-    {
-        if !super::cache::GUILD_MEMBER_ACTIONS_CACHE.contains_key(&guild_id) {
-            super::cache::GUILD_MEMBER_ACTIONS_CACHE.insert(guild_id, std::collections::HashMap::new());
-        }
+        if response.len() as i32 > guild_limit.limit_per {
+            // Cut off the latest limit_per(number) of actions from response
+            let mut actions = response.clone();
+            actions.sort_by(|a, b| a.created_at.cmp(&b.created_at));
+            actions.reverse();
+            actions.truncate(guild_limit.limit_per as usize);
+            // Now check if actions are done within the time limit
+            let mut last_time = actions[0].created_at;
+            let mut action_ids = Vec::new();
 
-        let Some(mut gm) = super::cache::GUILD_MEMBER_ACTIONS_CACHE.get_mut(&guild_id) else {
-            warn!("Guild not found in GUILD_MEMBER_ACTIONS_CACHE: {}", guild_id);
-            return Ok(());
-        };
-
-        let umac = gm.entry(user_id).or_insert_with(std::collections::HashMap::new);
-
-        if let std::collections::hash_map::Entry::Vacant(e) = umac.entry(ha.limit) {
-            e.insert(super::cache::GuildMemberCurrentActions {
-                    times: indexmap::indexmap! {
-                        sqlx::types::chrono::Utc::now().timestamp() => super::cache::TimesResolution {
-                            target: target.clone(),
-                            limits: Vec::new(),
-                            action_data: action_data.clone()
-                        }
-                    },
-                    time_action_map: dashmap::DashMap::new(),
-                    hit_limits: dashmap::DashMap::new(),
-                });
-        } else {
-            let Some(entry) = umac.get_mut(&ha.limit) else {
-                warn!("Limit not found in GUILD_MEMBER_ACTIONS_CACHE: {}", ha.limit);
-                return Ok(());
-            };
-
-            entry.times.insert(sqlx::types::chrono::Utc::now().timestamp(), super::cache::TimesResolution {
-                target: target.clone(),
-                limits: Vec::new(),
-                action_data: action_data.clone()
-            });
-        }
-    }
-    let Some(gmltm) = super::cache::GUILD_MEMBER_ACTIONS_CACHE.get(&guild_id) else {
-        warn!("Guild not found in GUILD_MEMBER_ACTIONS_CACHE: {}", guild_id);
-        return Ok(());
-    };
-
-    let Some(gmultm) = gmltm.get(&user_id) else {
-        warn!("User not found in GUILD_MEMBER_ACTIONS_CACHE: {}", user_id);
-        return Ok(());
-    };
-
-    /*if ignore_handling(pool, ha).await? {
-        // debug!("Ignoring handling [limit={}, user_id={}, target={}]", limit, user_id, target);
-        return Ok(());
-    }*/
-    // Check if they hit any limits yet
-    let hit = core::CurrentUserLimitsHit::newly_hit(guild_id, user_id, &guild_cache, &gmultm.clone());
-    drop(gmltm);
-    
-    // SAFETY: Tx should be dropped if error occurs, so make a scope to seperate tx queries
-    {
-        let mut tx: sqlx::Transaction<'_, sqlx::Postgres> = pool.begin().await?;
-
-        // Insert into limits__user_actions
-        sqlx::query!(
-            "
-            INSERT INTO limits__user_actions 
-            (action_id, guild_id, user_id, target, limit_type, action_data)
-            VALUES ($1, $2, $3, $4, $5, $6)
-        ",
-            crate::impls::crypto::gen_random(48),
-            guild_id.to_string(),
-            user_id.to_string(),
-            target,
-            limit.to_string(),
-            action_data
-        )
-        .execute(&mut *tx)
-        .await?;
-
-        for hit_limit in hit {
-            let Some(limit) = guild_cache.limits.get(&hit_limit.limit_id) else {
-                warn!("Limit not found in cache: {}", hit_limit.limit_id);
-                continue
-            };
-
-            // We have a hit limit for this user
-            info!("Hit limit: limit={:?}, hit={:?}", limit, hit_limit);
-
-            // Immediately handle the limit
-            let cur_uid = cache_http.cache.current_user().id;
-            let can_mod = {
-                let guild = cache_http.cache.guild(guild_id).ok_or("Guild not found")?;
-
-                guild.greater_member_hierarchy(cache_http.cache.clone(), cur_uid, user_id)
+            for action in actions {
+                if (last_time - action.created_at).num_seconds() < guild_limit.limit_time {
+                    action_ids.push(action.action_id.clone());
+                }
             }
-            .unwrap_or(cur_uid);
-            if can_mod == cur_uid {
-                info!("Moderating user");
-                match limit.limit_action {
-                    core::UserLimitActions::RemoveAllRoles => {
-                        // Get all user roles
-                        if let Ok(member) = guild_id.member(cache_http, user_id).await {
-                            let roles = member.roles.clone();
-                            for role in roles.iter() {
-                                if let Err(e) = member.remove_role(&cache_http.http, *role).await {
-                                    error!("Failed to remove role: {}", e);
+            if action_ids.len() as i32 > guild_limit.limit_per {
+                // remove from cache here
+                {
+                    let mut tx: sqlx::Transaction<'_, sqlx::Postgres> = pool.begin().await?;
+
+                    info!("Hit limit: limit={:?}", limit);
+
+                    // Immediately handle the limit
+                    let cur_uid = cache_http.cache.current_user().id;
+                    let can_mod = {
+                        let guild = cache_http.cache.guild(guild_id).ok_or("Guild not found")?;
+
+                        guild.greater_member_hierarchy(cache_http.cache.clone(), cur_uid, user_id)
+                    }
+                        .unwrap_or(cur_uid);
+                    if can_mod == cur_uid {
+                        info!("Moderating user");
+                        match guild_limit.limit_action {
+                            core::UserLimitActions::RemoveAllRoles => {
+                                // Get all user roles
+                                if let Ok(member) = guild_id.member(cache_http, user_id).await {
+                                    let roles = member.roles.clone();
+                                    for role in roles.iter() {
+                                        if let Err(e) = member.remove_role(&cache_http.http, *role).await {
+                                            error!("Failed to remove role: {}", e);
+                                        }
+                                    }
+                                }
+                            }
+                            core::UserLimitActions::KickUser => {
+                                if let Err(e) = guild_id.kick(&cache_http.http, user_id).await {
+                                    error!("Failed to kick user: {}", e);
+                                }
+                            }
+                            core::UserLimitActions::BanUser => {
+                                if let Err(e) = guild_id.ban(&cache_http.http, user_id, 0).await {
+                                    error!("Failed to kick user: {}", e);
                                 }
                             }
                         }
+                    } else {
+                        warn!("Cannot moderate user, not enough permissions: {}, {}",
+                            can_mod, cur_uid
+                        );
+
+                        // Add UserActions to db Here.
+
+                        sqlx::query!("
+                            INSERT INTO limits__past_hit_limits
+                            (id, guild_id, user_id, limit_id, cause, notes)
+                            VALUES ($1, $2, $3, $4, $5, $6)",
+                            crate::impls::crypto::gen_random(16),
+                            guild_id.to_string(), user_id.to_string(),
+                            limit_id, &action_ids,
+                            &vec!["Not enough permissions to moderate user".to_string()]
+                        )
+                            .execute(&mut *tx)
+                            .await?;
+
+                        return Ok(());
                     }
-                    core::UserLimitActions::KickUser => {
-                        if let Err(e) = guild_id.kick(&cache_http.http, user_id).await {
-                            error!("Failed to kick user: {}", e);
-                        }
-                    }
-                    core::UserLimitActions::BanUser => {
-                        if let Err(e) = guild_id.ban(&cache_http.http, user_id, 0).await {
-                            error!("Failed to kick user: {}", e);
-                        }
-                    }
+
+                    // Add UserActions to db Here.
+
+                    sqlx::query!("
+                        INSERT INTO limits__past_hit_limits
+                        (id, guild_id, user_id, limit_id, cause)
+                        VALUES ($1, $2, $3, $4, $5)",
+                        crate::impls::crypto::gen_random(16),
+                        guild_id.to_string(), user_id.to_string(),
+                        limit_id, &action_ids
+                    ).execute(&mut *tx)
+                        .await?
                 }
-            } else {
-                warn!(
-                    "Cannot moderate user, not enough permissions: {}, {}",
-                    can_mod, cur_uid
-                );
-
-                let Some(user_map) = super::cache::GUILD_MEMBER_ACTIONS_CACHE.get(&guild_id) else {
-                    warn!("Guild not found in GUILD_MEMBER_ACTIONS_CACHE: {}", guild_id);
-                    continue;
-                };
-                let Some(action_map) = user_map.get(&user_id) else {
-                    warn!("User not found in GUILD_MEMBER_ACTIONS_CACHE: {}", user_id);
-                    continue;
-                };
-
-                let Some(gmac) = action_map.get(&limit.limit_type) else {
-                    warn!("Limit not found in GUILD_MEMBER_ACTIONS_CACHE: {}", limit.limit_type);
-                    continue;
-                };
-
-                let action_ids = gmac.sync_with_db(pool, limit.limit_type, user_id, guild_id).await?;
-
-                sqlx::query!(
-                    "
-                INSERT INTO limits__past_hit_limits
-                (id, guild_id, user_id, limit_id, cause, notes)
-                VALUES ($1, $2, $3, $4, $5, $6)",
-                    crate::impls::crypto::gen_random(16),
-                    guild_id.to_string(),
-                    user_id.to_string(),
-                    hit_limit.limit_id,
-                    &action_ids,
-                    &vec!["Not enough permissions to moderate user".to_string()]
-                )
-                .execute(&mut *tx)
-                .await?;
-
-                return Ok(());
             }
-
-            for action in hit_limit.cause.iter() {
-                sqlx::query!(
-                    "
-                UPDATE limits__user_actions
-                SET limits_hit = array_append(limits_hit, $1)
-                WHERE action_id = $2",
-                    hit_limit.limit_id,
-                    action.action_id
-                )
-                .execute(&mut *tx)
+        } else {
+            let _ = cache.create::<Vec<UserAction>>("user_actions")
+                .content(UserAction {
+                    action_id: crate::impls::crypto::gen_random(48),
+                    guild_id,
+                    user_id,
+                    target: target.clone(),
+                    limit_type: limit,
+                    action_data: action_data.clone(),
+                    created_at: sqlx::types::chrono::Utc::now(),
+                    limits_hit: Vec::new(),
+                })
                 .await?;
-            }
-
-            sqlx::query!(
-                "
-            INSERT INTO limits__past_hit_limits
-            (id, guild_id, user_id, limit_id, cause)
-            VALUES ($1, $2, $3, $4, $5)",
-                crate::impls::crypto::gen_random(16),
-                guild_id.to_string(),
-                user_id.to_string(),
-                hit_limit.limit_id,
-                &hit_limit
-                    .cause
-                    .iter()
-                    .map(|a| a.action_id.clone())
-                    .collect::<Vec<_>>()
-            )
-            .execute(&mut *tx)
-            .await?;
         }
-
-        tx.commit().await?;
     }
-
     Ok(())
 }
