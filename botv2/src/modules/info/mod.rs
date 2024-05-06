fcfeff0b6bd0855268c465d9e4787de4dd9ae2dd
a <a@a.com>
2024-05-06 14:58:25 +0000

    add basic whois command

--- botv2/src/modules/info/mod.rs
+++ botv2/src/modules/info/mod.rs
@@ -1,16 +1,19 @@
-use futures_util::FutureExt;
+pub mod whois;
 
 pub fn module() -> crate::silverpelt::Module {
     crate::silverpelt::Module {
         id: "info",
         name: "Info Commands",
         description: "Get information about the server/guilds etc. Useful for diagnostics",
-        toggleable: false,
-        commands_configurable: false,
+        toggleable: true,
+        commands_configurable: true,
         virtual_module: false,
         web_hidden: false,
         is_default_enabled: true,
-        commands: vec![],
+        commands: vec![(
+            whois::whois(),
+            crate::silverpelt::CommandExtendedData::none(),
+        )],
         ..Default::default()
     }
 }
