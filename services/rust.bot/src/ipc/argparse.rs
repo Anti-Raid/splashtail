b6828734852cc5406da8b283af63b3aaed1964d4
a <a@a.com>
2024-11-30 05:01:14 +0000

    f

--- services/rust.bot/src/ipc/argparse.rs
+++
@@ -1,9 +0,0 @@
-use clap::Parser;
-
-#[derive(Parser, Debug, Clone)]
-pub struct CmdArgs {
-    #[clap(long)]
-    pub shards: Option<Vec<u16>>,
-    #[clap(long)]
-    pub shard_count: Option<u16>,
-}
