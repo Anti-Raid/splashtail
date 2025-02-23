4d0b786574f5cc3a7fae40b9117d8e29c66d5f86
a <a@a.com>
2025-02-23 12:02:10 -0500

    begin adding seaweed

---
+++ data/docker/seaweed/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Copy filer.toml to /etc/seaweedfs/filer.toml if /etc/seaweedfs/filer.toml does not exist
+ls /etc/seaweedfs
+if [ ! -f /etc/seaweedfs/filer.toml ]; then \
+    cp /filer.toml /etc/seaweedfs/filer.toml; \
+    else \
+    echo "/etc/seaweedfs/filer.toml already exists, not overwriting"; \
+    fi
+
+# Same with replication.toml
+if [ ! -f /etc/seaweedfs/replication.toml ]; then \
+    cp /replication.toml /etc/seaweedfs/replication.toml; \
+    else \
+    echo "/etc/seaweedfs/replication.toml already exists, not overwriting"; \
+    fi
+
+# We only care about server
+case "$1" in
+  'server')
+  	ARGS="-dir=/data -volume.max=0 -master.volumePreallocate -master.volumeSizeLimitMB=4096 -filer.encryptVolumeData"
+ 	shift
+  	exec /usr/bin/weed -logtostderr=true server $ARGS $@
+  	;;
+
+  'shell')
+  	ARGS="-cluster=$SHELL_CLUSTER -filer=$SHELL_FILER -filerGroup=$SHELL_FILER_GROUP -master=$SHELL_MASTER -options=$SHELL_OPTIONS"
+  	shift
+  	exec echo "$@" | /usr/bin/weed -logtostderr=true shell $ARGS
+  ;;
+
+  *)
+  	exec /usr/bin/weed $@
+	;;
+esac
