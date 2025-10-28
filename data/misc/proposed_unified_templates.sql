af59bf009dd5ae8aa4e2ee23be6d39161685cd47
Sanandan Sashikumar <cheesycod@outlook.com>
2025-10-28 06:07:57 +0100

    fix

--- data/misc/proposed_unified_templates.sql
+++ data/misc/proposed_unified_templates.sql
@@ -18,34 +18,27 @@ CREATE TABLE template_pool (
     state TEXT NOT NULL DEFAULT 'active', -- 'active', 'paused', 'suspended'
 );
 
-CREATE INDEX template_pool_idx ON template_pool (id, name, owner_type, owner_id, language, created_at, paused, content);
+CREATE INDEX template_pool_idx ON template_pool (id, owner_type, owner_id, language, created_at, state);
 
 -- Template shop listings, these reference templates in the pool
 CREATE TABLE template_shop_listings (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name TEXT PRIMARY KEY,
-    template_pool_ref UUID REFERENCES template_pool(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    template_pool_ref UUID NOT NULL UNIQUE REFERENCES template_pool(id) ON UPDATE CASCADE ON DELETE RESTRICT,
     review_state TEXT NOT NULL DEFAULT 'pending', -- 'pending', 'approved', 'denied'
     default_events TEXT[] NOT NULL DEFAULT '{}'::text[],
     default_allowed_caps TEXT[] NOT NULL DEFAULT '{}'::text[],
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    last_updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    last_updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-CREATE INDEX template_shop_idx ON template_shop (id, name, template_pool_ref, review_state, created_at);
+CREATE INDEX template_shop_idx ON template_shop (id, template_pool_ref, review_state, created_at);
 
 -- Guild owned templates
-CREATE TABLE guild_templates (
+CREATE TABLE guild_templates(
     guild_id TEXT NOT NULL,
-    template_pool_ref UUID REFERENCES template_pool(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    template_pool_ref UUID NOT NULL REFERENCES template_pool(id) ON UPDATE CASCADE ON DELETE RESTRICT,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    allowed_caps TEXT[] NOT NULL DEFAULT '{}'::text[],
+    events TEXT[] NOT NULL DEFAULT '{}'::text[],
     PRIMARY KEY (guild_id, template_pool_ref)
-);
-
--- Guild shop templates
-CREATE TABLE guild_shop_templates (
-    guild_id TEXT NOT NULL,
-    template_shop_ref UUID REFERENCES template_shop_listings(id) ON UPDATE CASCADE ON DELETE RESTRICT,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    PRIMARY KEY (guild_id, template_shop_ref)
 );
