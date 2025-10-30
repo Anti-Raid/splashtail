2665de30941170330742a40be0765ef5bbfe9624
Sanandan Sashikumar <cheesycod@outlook.com>
2025-10-30 05:28:34 +0100

    fix

--- data/misc/proposed_unified_templates.sql
+++ data/misc/proposed_unified_templates.sql
@@ -1,3 +1,13 @@
+-- Proposed Unified Template 'RFC'/project
+--
+-- This project moves content, ownership data and state
+-- to its own table called template_pool with guild attached templates
+-- and listings in the shop referencing the pool.
+--
+-- This enables for new features such as:
+-- - Directly publishing a guild template to the shop
+-- without needing to duplicate content
+
 -- The template pool which stores all templates
 CREATE TABLE template_pool (
     -- Identifier for the template in the pool
@@ -22,9 +32,8 @@ CREATE INDEX template_pool_idx ON template_pool (id, owner_type, owner_id, langu
 
 -- Template shop listings, these reference templates in the pool
 CREATE TABLE template_shop_listings (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    template_pool_ref UUID PRIMARY KEY UNIQUE REFERENCES template_pool(id) ON UPDATE CASCADE ON DELETE CASCADE,
     short TEXT NOT NULL,
-    template_pool_ref UUID NOT NULL UNIQUE REFERENCES template_pool(id) ON UPDATE CASCADE ON DELETE RESTRICT,
     review_state TEXT NOT NULL DEFAULT 'pending', -- 'pending', 'approved', 'denied'
     default_events TEXT[] NOT NULL DEFAULT '{}'::text[],
     default_allowed_caps TEXT[] NOT NULL DEFAULT '{}'::text[],
@@ -37,8 +46,8 @@ CREATE INDEX template_shop_idx ON template_shop (id, template_pool_ref, review_s
 -- Guild attached templates
 CREATE TABLE attached_guild_templates(
     guild_id TEXT NOT NULL,
-    template_pool_ref UUID NOT NULL REFERENCES template_pool(id) ON UPDATE CASCADE ON DELETE RESTRICT,
-    shop_listing_ref UUID REFERENCES template_shop_listings(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    template_pool_ref UUID NOT NULL REFERENCES template_pool(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    source TEXT NOT NULL, -- Source of how this template was attached (e.g., 'shop_listing', 'created', etc.)
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     allowed_caps TEXT[] NOT NULL DEFAULT '{}'::text[],
     events TEXT[] NOT NULL DEFAULT '{}'::text[],
